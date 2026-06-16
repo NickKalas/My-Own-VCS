6ac6a808ba1b77422365ca17f2060537cf54ab00
Νικος Καλαμπακας <kalampakasnick@gmail.com>
2026-06-16 17:48:27 +0300

    V.1

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import hashlib
 import zlib
+import os
+from pathlib import Path
 from database import update_1st_table
 
 def stage_file(file_path: str) -> None:
@@ -19,5 +21,24 @@ def stage_file(file_path: str) -> None:
     except Exception as e:
         print(f"An unexpected error occurred while staging {file_path}: {e}")
 
+def scan_directory(dir_path: str) -> str:
+    try:
+        for item in os.scandir(dir_path):
+            file_path = os.path.abspath(Path(item))
+            item_name = os.path.basename(file_path)
+
+            if item_name.startswith('.') or item_name == "database.db":
+                continue
+            elif item.is_file():
+                stage_file(file_path)
+            elif item.is_dir():
+                scan_directory(file_path)
+            else:
+                print("Something did not go well, try again!!!")
+    except Exception as e:
+        print(f"An error occured: {e}")
+        quit()
+
 if __name__ == "__main__":
-    stage_file("test.txt")
+    folder_path = Path("C:\\Users\\nikos\\OneDrive\\Desktop\\Automation\\my_own_git")
+    scan_directory(folder_path)
